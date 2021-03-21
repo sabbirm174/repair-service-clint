060de07210e09cd7b74d7ae3d908a81c8db0f96d
Sabbir Alam <sabbirm174@gmail.com>
2021-03-21 19:12:31 +0600

    validation

--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -4,15 +4,26 @@ import "firebase/auth";
 import "firebase/firestore";
 import firebaseConfig from '../../config.firebase';
 import { MyContext } from './../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './signUp.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGoogle } from "@fortawesome/free-brands-svg-icons"
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+
 
 const SignUp = () => {
     const [loggedInUser, setLoggedInUser] = useContext(MyContext)
     if(firebase.apps.length === 0){
         firebase.initializeApp(firebaseConfig);
     }
-    const provider =  new firebase.auth.GoogleAuthProvider();
+
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const facebookProvider = new firebase.auth.FacebookAuthProvider();
     
     const [user,setUser] = useState({
         isSignin: false,
@@ -26,25 +37,6 @@ const SignUp = () => {
         matchPassword:'',
         success:false
     })
-
-    //google sign up
-    const googleSubmit = () =>{
-        firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-            const credential = result.credential;
-
-            const token = credential.accessToken;
-            const user = result.user;
-            // ...
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.email;
-            const credential = error.credential;
-        });
-
-    }
 
     //form validation
     const handleFormValidation = (e) =>{
@@ -64,14 +56,7 @@ const SignUp = () => {
             newUserInfo[e.target.name] = e.target.value;
             setUser(newUserInfo);
         }
-
-        
     }
-
-
-
-
-
 
     // sign up with email and password
     const handleSubmit = (e) =>{
@@ -79,24 +64,13 @@ const SignUp = () => {
 
         if(user.email){
             // remove success message for next time sign up
-            const newError = {...user}
+            
             newError.success = false;
-            setUser(newError);
-
-            const isNotMatched = {...user};
-            isNotMatched.matchPassword = ""
-            setUser(isNotMatched);
-
-            const isNotEmail = {...user};
-            isNotEmail.invalidEmail = "";
-            setUser(isNotEmail);
+            newError.matchPassword = ""
+            newError.invalidEmail = "";
 
             if(user.password === user.confirmPassword){
-
-                const isNotEmail = {...user};
-                isNotEmail.invalidEmail = "";
-                setUser(isNotEmail);
-
+                newError.invalidEmail = "";
                 firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
                 .then((userCredential) => {
                 const user = userCredential.user;
@@ -115,34 +89,87 @@ const SignUp = () => {
             });
             }// password finish
             else{
-                const isNotMatched = {...user};
-                isNotMatched.matchPassword = `password and confirm password did not matchd or <br/> invalid password please type at least 6 charecter and a numbur`
-                setUser(isNotMatched); 
+                newError.matchPassword = `password and confirm password did not matchd`
             }
         }//email finish
         else{
-            const isNotEmail = {...user};
-            isNotEmail.invalidEmail = `please enter a valid email address`
-            setUser(isNotEmail);
+            newError.invalidEmail = `please enter a valid email address`
         }
         e.preventDefault()
+        setUser(newError);
     }
 
-     
+     // google sign in
+    const googleSignIn = () =>{
+        firebase.auth()
+  .signInWithPopup(googleProvider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+    var token = credential.accessToken;
+    const { displayName, email} = result.user;
+    const userInfo = {name: displayName, email};
+    setLoggedInUser(userInfo);
+    history.replace(from);
+    console.log(userInfo)
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+  });
+    }
+
+    //facebook log in
+
+    const handleFacebookLogin = ()=>{
+        firebase
+  .auth()
+  .signInWithPopup(facebookProvider)
+  .then((result) => {
+     var credential = result.credential;
+    var user = result.user;
+    const displayName = user.displayName;
+    const email = user.email
+    const userInfo = {name: displayName, email};
+    setLoggedInUser(userInfo);
+    var accessToken = credential.accessToken;
+    console.log(userInfo);
+    history.replace(from);
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log(errorMessage)
+    // ...
+  });
+    }
     
     return (
-        <div className='d-flex height justify-content-center align-items-center'>
+        <div className='d-flex sign-up-wrapper justify-content-center align-items-center'>
             <div className= 'cu-border'>
-                <form action="" onSubmit={handleSubmit}>
+                <h2>Create Your Account</h2>
+                <form action="" onSubmit={handleSubmit} className="text-center">
                     <br/><input type="text" placeholder="name" name='name' onBlur={handleFormValidation} required/><br/>
                     <br/><input type="text" placeholder="email" name='email' onBlur={handleFormValidation} required/><br/>
                     <br/><input type="password" placeholder="password" name='password' onBlur={handleFormValidation} required/><br/>
                     <br/><input type="password" placeholder="confirm password" name='confirmPassword' onBlur={handleFormValidation} required/><br/>
-                    <input className='btn btn-primary' type="submit"/>
+                    <input className='btn btn-primary submit' value="create an account" type="submit"/>
+                    <h6>Already Have an account <Link to="/login">Log in</Link></h6>
                 </form>
-                <button className='btn btn-primary'onClick={googleSubmit}>google</button>
+
+                <div className="text-center social-btn">
+                    <h6>or, sign in with</h6>
+                    <button className="btn btn-primary" onClick={googleSignIn}><FontAwesomeIcon className="fontawsome" icon={faGoogle} /> Continue with google</button><br/>
+                    <button className="btn btn-primary" onClick={handleFacebookLogin}><FontAwesomeIcon className="fontawsome" icon={faFacebook} /> Continue with facebook</button>
+                </div>
+
+                
+                
                 <p style={{color:"red"}}>{user.error}</p>
-                <Link to="/login">Log in</Link>
                 {user.success && <p style={{color:'green'}}>user created successfully</p> }
                 {<p style={{color:"red"}}>{user.matchPassword}</p>}
                 {<p style={{color:"red"}}>{user.invalidEmail}</p>}
